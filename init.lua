a8e344870ccf4fe69221f703f96ff857de1612b3
Tokosan <cteiba.al@gmail.com>
2025-10-30 13:33:36 -0300

    [plugin] + noice

--- init.lua
+++ init.lua
@@ -1009,6 +1009,20 @@ require('lazy').setup({
     end,
   },
   {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      'MunifTanjim/nui.nvim',
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      'rcarriga/nvim-notify',
+    },
+  },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the

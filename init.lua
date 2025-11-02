03955e9f9d583795a95809bfaebf0792d79130a9
Tokosan <cteiba.al@gmail.com>
2025-11-02 18:06:51 -0300

    plugin: ~ copilot

--- init.lua
+++ init.lua
@@ -974,7 +974,27 @@ require('lazy').setup({
     },
     cmd = 'Copilot',
     event = 'InsertEnter',
-    config = true,
+    config = function()
+      require('copilot').setup {
+        -- Your Copilot configuration options here
+        -- For example:
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          keymap = {
+            accept = '<C-y>',
+            next = '<C-n>',
+            prev = '<C-p>',
+            dismiss = '<C-x>',
+          },
+        },
+        panel = {
+          enabled = true,
+          auto_refresh = true,
+        },
+        -- ... other options as per copilot.lua documentation
+      }
+    end,
   },
   {
     'ThePrimeagen/harpoon',
